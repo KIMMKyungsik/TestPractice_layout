ad0c74f3b989e3685262ec9b97b38ca2f69e2382
KIMMKyungsik <93589990+KIMMKyungsik@users.noreply.github.com>
2021-11-30 04:52:39 +0900

    Initial commit

---
+++ app/src/main/java/org/techtown/DataAdapter.kt
@@ -0,0 +1,8 @@
+package org.techtown
+
+import android.content.Context
+import androidx.recyclerview.widget.RecyclerView
+
+class DataAdapter{
+
+}
---
+++ app/src/main/java/org/techtown/recyclerviewprac/MainActivity.kt
@@ -0,0 +1,24 @@
+package org.techtown.recyclerviewprac
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+
+
+
+
+
+
+}
+
